224244b1b23145663837e541599cb4cf3575f3cd
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-04-10 11:20:53 +0200

    lagt till mall för sidor

--- FrontEndCanvax/src/main.js
+++ FrontEndCanvax/src/main.js
@@ -17,10 +17,17 @@ import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
+
+/* Filips layout */
+import BaseLayout from './components/BaseLayout.vue'
 const app = createApp(App)
     .use(IonicVue)
     .use(router);
-router.isReady().then(() => {
+
+app.component('base-layout', BaseLayout);
+
+
+    router.isReady().then(() => {
     app.mount('#app');
 });
 //# sourceMappingURL=main.js.map
--- FrontEndCanvax/src/views/Tab4Page.vue
+++ FrontEndCanvax/src/views/Tab4Page.vue
@@ -1,22 +1,18 @@
 <template>
-  <ion-page>
-    <ion-header>
-     <ion-toolbar>
-      <ion-title>Importera kalender</ion-title>
-    </ion-toolbar>
-    </ion-header>
-    <ion-content>
+ <base-layout page-title="Kurser">
        <ion-list>
-        <h2> Välj kurs nedan: </h2>
-  <ion-item>da339a</ion-item>
-  <ion-item>da211a</ion-item>
-  <ion-item>da297a</ion-item>
+          <ion-item>da339a</ion-item>
+          <ion-item>da211a</ion-item>
+          <ion-item>da297a</ion-item>
        </ion-list> 
-    </ion-content>
-  </ion-page>
+      </base-layout>
 </template>
 
-<script setup lang="ts">
-  import {IonList,IonPage, IonHeader, IonTitle, IonContent, IonToolbar} from '@ionic/vue';
+<script>
+ import { IonList, IonItem} from '@ionic/vue';
+
+ export default {
+    components: { IonList, IonItem },
+  };
 
 </script>
