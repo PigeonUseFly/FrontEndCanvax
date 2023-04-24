f3f7ecdb36a33c7d58d21fb5d305677edf9dad1b
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-04-24 15:44:19 +0200

    ny

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>File loader</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+    <ion-fab-button @click="takePhoto()">
+      <ion-icon :icon="fileTrayFullOutline"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { camera, trash, close, fileTrayFullOutline } from 'ionicons/icons';
+import {
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+</script>
