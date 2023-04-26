dd48b0910164843008200561a0f1f8914675f034
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2023-04-26 10:15:35 +0200

    ddd

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,7 +4,7 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="calendarNumber" />
           <ion-label>Calender</ion-label>
         </ion-tab-button>
 
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { arrowDown, square, triangle } from 'ionicons/icons';
+import { arrowDown, calendarNumber, square} from 'ionicons/icons';
 </script>
