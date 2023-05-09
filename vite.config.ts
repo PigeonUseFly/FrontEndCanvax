0ad625a3387987effd0f9e810fff46120b9a243d
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-05-09 10:37:42 +0200

    Muskavlar

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["@stencil/core/internal/client"]
+    }
+  },
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
