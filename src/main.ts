134e0fcc914208e6a0b4c97d44796aee8805c904
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-05-23 16:12:46 +0200

    Revert "Förösker få komponenten att fungera"
    This reverts commit 2f142bc5b97e6836640123d2bf2547c802491660.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
-/*Filips dialog saker*/
-
-import Vue from 'vue';
-import VuejsDialog from 'vuejs-dialog';
-import 'vuejs-dialog/dist/vuejs-dialog.min.css';
-import 'vuejs-dialog/dist/vuejs-dialog-mixin.min.js';
-
-/*Filips dialog saker ovan*/
-
-
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -41,14 +31,11 @@ import 'v-calendar/style.css';
 /*Filips calender saker ovan*/
 
 
-
-
 const app = createApp(App)
   .use(IonicVue)
   .use(router)
   .use(VCalendar, {}) // Filip la till denna rad och tog bort ;
-  .use(Vuetify)
-
+  
   
 router.isReady().then(() => {
   app.mount('#app');
