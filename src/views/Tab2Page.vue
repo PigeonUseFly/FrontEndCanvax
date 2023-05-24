4b222f285ae08c5847e725cf2ea24b6c7c77eb72
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2023-05-24 15:32:52 +0200

    ,.,.,.

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -9,23 +9,33 @@
       <button type="button" class="btn btn-primary" @click="showModal = true">
         Open a modal
       </button>
-      <Modal v-model="showModal" title="My first modal">
+      <SavedModal v-model="showModal" title="My first modal">
         <p>Modal content goes here...</p>
-      </Modal>
+      </SavedModal>
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
       <ExploreContainer name="Tab 2 page" />
+    <SavedModal v-show="showModal" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import SavedModal from '@/components/SavedModal.vue';
 
 let showModal = false; // Define the showModal variable to control modal visibility
 
+export default {
+  components: { SavedModal },
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+}
 </script>
