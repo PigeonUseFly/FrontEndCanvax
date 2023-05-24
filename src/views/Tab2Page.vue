ce38ad4412e25780336c268b38118661a2b43eb7
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2023-05-24 15:10:31 +0200

    ,.,.,.,

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,36 +2,30 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>File loader</ion-title>
+        <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-    <ion-fab-button @click="takePhoto()">
-      <ion-icon :icon="fileTrayFullOutline"></ion-icon>
-    </ion-fab-button>
-  </ion-fab>
+      <button type="button" class="btn btn-primary" @click="showModal = true">
+        Open a modal
+      </button>
+      <Modal v-model="showModal" title="My first modal">
+        <p>Modal content goes here...</p>
+      </Modal>
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      
+      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { camera, trash, close, fileTrayFullOutline } from 'ionicons/icons';
-import {
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
-} from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+let showModal = false; // Define the showModal variable to control modal visibility
+
 </script>
