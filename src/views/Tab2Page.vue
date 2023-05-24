086ed5229091cfc8fe7adcbafa4b481fd8608ea8
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2023-05-24 14:37:06 +0200

    Nu jävlar!!!!!!!!!!!!!!!!!

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,18 +1,18 @@
 <template>
   <ion-page>
     <ion-header>
-      <Button label="Show" icon="pi pi-external-link" @click="visible = true" />
+     
       <ion-toolbar>
         <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-</Dialog>
+      <button type="button" class="btn btn-primary" @click="showModal=true">
+  Open a modal
+</button>
+<Modal v-model="showModal" title="My first modal">
+  <p>Modal content goes here...</p>
+</Modal>
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
@@ -25,7 +25,9 @@
 </template>
 
 <script setup lang="ts">
-import Dialog from 'primevue/dialog';
+
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+
+
 </script>
