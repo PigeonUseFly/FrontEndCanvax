3f406117d1d2f227ccca4062fa6649d65990ef0e
TabascoTwan <antonlokesandell@gmail.com>
2023-05-26 14:29:55 +0200

    Byggt vidare på Create event fliken
    90% färdigt, behöver bara lösa problemet med parsing av datum

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,41 +1,73 @@
 <template>
   <IonPage>
-  <ion-list>
-    <ion-item>
-      <ion-input label="Create event" value="" :readonly="true"></ion-input>
-    </ion-item>
+    <ion-list>
+      <ion-item>
+        <ion-input label="Create event" value="" :readonly="true"></ion-input>
+      </ion-item>
 
-    <ion-item>
-      <ion-input ref="idInput" label="ID: " placeholder="Enter ID"></ion-input>
-    </ion-item>
+      <ion-item>
+        <ion-input ref="summaryInput" label="Summary:" placeholder="Enter short summary" v-model="summaryInput"></ion-input>
+      </ion-item>
 
-    <ion-item>
-      <ion-input ref="titleInput" label="Title:" placeholder="Enter event title"></ion-input>
-    </ion-item>
+      <ion-item>
+        <ion-input ref="descriptionInput" label="Description:" placeholder="Enter detailed description" v-model="descriptionInput"></ion-input>
+      </ion-item>
 
-    <ion-item>
-      <ion-input ref="startDateInput" label="Starting date: " placeholder="Enter date"></ion-input>
-    </ion-item>
+      <ion-item>
+        <ion-input ref="startDateInput" type="datetime-local" label="Starting date: " v-model="startDateInput"></ion-input>
+      </ion-item>
 
-    <ion-item>
-      <ion-input ref="endDateInput" label="Ending date: " placeholder="Enter date"></ion-input>
-    </ion-item>
+      <ion-item>
+        <ion-input ref="endDateInput" type="datetime-local" label="Ending date: " v-model="endDateInput"></ion-input>
+      </ion-item>
 
-  
-  </ion-list>
-</IonPage>
+      <ion-item>
+        <ion-input ref="locationInput" label="Location: " placeholder="Enter location" v-model="locationInput"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-button expand="full" @click="handleSubmit">Add event</ion-button>
+      </ion-item>
+    </ion-list>
+  </IonPage>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { camera, trash, close, fileTrayFullOutline } from 'ionicons/icons';
-import {
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
-} from '@ionic/vue';
+import { IonPage } from '@ionic/vue';
+import { ref } from 'vue';
+
+const summaryInput = ref('');
+const descriptionInput = ref('');
+const startDateInput = ref('');
+const endDateInput = ref('');
+const locationInput = ref('');
+
+const handleSubmit = async () => {
+  const summary = summaryInput.value.toString();
+  const description = descriptionInput.value.toString();
+  const startDateString = startDateInput.value;
+  const endDateString = endDateInput.value;
+  const location = locationInput.value.toString();
+
+  const startDate = new Date(startDateString);
+  const endDate = new Date(endDateString);
+
+  try {
+    await fetch("http://localhost:8080/events/insert", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        summary,
+        description,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        location,
+      }),
+    });
+  } catch (error) {
+    console.error(error)
+  }
+};
 </script>
